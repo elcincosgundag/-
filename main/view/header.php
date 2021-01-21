561dc5b0a7865568debd4999366e6b8fb31d8e06
elcincosgundag <77799596+elcincosgundag@users.noreply.github.com>
2021-01-21 21:23:20 +0300

    Update header.php

--- main/view/header.php
+++ main/view/header.php
@@ -1,14 +1,9 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <head>
-    <meta charset="utf-8">
-    <title>Тестовое задание</title>
-    <link rel="stylesheet" href="/other/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/other/css/main.css">
-    <link rel="stylecss"type=text/css href="css/styles.css">
-</head>
-<body>
-<div id="header">
-</body>
-</html>
-
+ <meta charset="utf-8">
+<title> Тестовое задание </title>
+<link href="css/style.css" media="screen" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'rel='stylesheet' type='text/css'>
+	</head>
 	
+   <body>
