f9bb87763c7676cfc0246cc559f648e815210c0b
elcincosgundag <77799596+elcincosgundag@users.noreply.github.com>
2021-01-21 21:21:42 +0300

    Update hello.php

--- main/view/site/hello.php
+++ main/view/site/hello.php
@@ -1,12 +1,9 @@
-<div class="text-center row">
-	<div class="col-sm-2"></div>
-	<div class="col-sm-8">
-
-        <div class="jumbotron">
-            <h3>Hello, <?= htmlspecialchars($_SESSION['name']);?>.</h3>
-        </div>
-        <a type="button" href="/site/logout" class="btn btn-success">выйти из профиля</a>
-
-	</div>
-	<div class="col-sm-2"></div>
+<?php include("includes/header.php"); ?>
+<div id="welcome">
+<h2>Добро пожаловать, <span><?php echo $_SESSION['session_username'];?>! </span></h2>
+  <p><a href="logout.php">Выйти</a> из системы</p>
 </div>
+	
+<?php include("includes/footer.php"); ?>
+	
+<?php endif; ?>
