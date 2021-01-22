1249551654bbe8b3661a089360ac09cba04e42e4
elcincosgundag <77799596+elcincosgundag@users.noreply.github.com>
2021-01-22 11:11:25 +0300

    Update header.php

--- main/view/header.php
+++ main/view/header.php
@@ -2,7 +2,7 @@
 <head>
  <meta charset="utf-8">
 <title> Тестовое задание </title>
-<link href="css/style.css" media="screen" rel="stylesheet">
+<link href="./other/css/style.css" media="screen" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'rel='stylesheet' type='text/css'>
 	</head>
 	
